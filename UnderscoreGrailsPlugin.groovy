961bfc13d93f6b54fed49ddbb8335e88c82acf45
Burt Beckwith <beckwithb@vmware.com>
2012-10-12 16:41:33 -0400

    minor cleanup

--- UnderscoreGrailsPlugin.groovy
+++ UnderscoreGrailsPlugin.groovy
@@ -2,21 +2,13 @@ class UnderscoreGrailsPlugin {
 
 	def version = '1.4.0'
 	def grailsVersion = '2.0 > *'
-	def dependsOn = [:]
-	def pluginExcludes = [
-			'grails-app/views/error.gsp'
-	]
-
 	def title = 'Underscore Plugin'
 	def author = 'Michael Stogowski'
 	def authorEmail = 'stogi86@gmail.com'
-	def description = '''\
-Simple plugin wrapper for useful Underscore.js library.
-'''
+	def description = 'Simple plugin wrapper for useful Underscore.js library.'
 
 	def documentation = 'https://github.com/stogi/grails-underscore'
 	def license = 'APACHE'
 	def issueManagement = [system: 'GitHub', url: 'https://github.com/stogi/grails-underscore/issues']
 	def scm = [url: 'https://github.com/stogi/grails-underscore']
-
 }
